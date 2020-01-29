fca7e472ba384f773010d1c9a6bd09cd1b400f9e
ole4kav <ole4kav@walla.co.il>
2020-01-29 23:49:32 +0200

    fix xml files

--- app/src/main/java/com/example/movieapplicationjava/fragments/MovieDetailsFragment.java
+++ app/src/main/java/com/example/movieapplicationjava/fragments/MovieDetailsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,10 +21,10 @@ public class MovieDetailsFragment extends Fragment implements MovieDetailsInterf
 
     public static final String KEY_MOVIE = "movie";
 
-    MovieDetailsInterface.Presenter presenter;
+    private MovieDetailsInterface.Presenter presenter;
 
     private ImageView ivMovieImage;
-    private TextView tvTitle, tvYear, tvRating, tvGenre;
+    private TextView tvTitle, tvYear, tvRating, tvGenre, tvTitleDescription, tvYearDescription, tvGenreDescription;
     private Movie movie;
 
 
@@ -48,18 +49,31 @@ public class MovieDetailsFragment extends Fragment implements MovieDetailsInterf
 
     private void initViews(View view) {
         ivMovieImage = view.findViewById(R.id.ivMovieImage);
-        tvTitle = view.findViewById(R.id.tvTitle);
-        tvYear = view.findViewById(R.id.tvYear);
         tvRating = view.findViewById(R.id.tvRating);
-        tvGenre = view.findViewById(R.id.tvGenre);
+
+        LinearLayout llTitle = view.findViewById(R.id.llTitle);
+        tvTitle = llTitle.findViewById(R.id.tvTitle);
+        tvTitleDescription = llTitle.findViewById(R.id.tvDescription);
+
+        LinearLayout llYear = view.findViewById(R.id.llYear);
+        tvYear = llYear.findViewById(R.id.tvTitle);
+        tvYearDescription = llYear.findViewById(R.id.tvDescription);
+
+        LinearLayout llGenre = view.findViewById(R.id.llGenre);
+        tvGenre = llGenre.findViewById(R.id.tvTitle);
+        tvGenreDescription = llGenre.findViewById(R.id.tvDescription);
     }
 
     @Override
     public void setData(Movie movie) {
-        tvTitle.setText(movie.getTitle());
-        tvYear.setText(String.valueOf(movie.getReleaseYear()));
+        tvTitle.setText(getString(R.string.movie_name_title));
+        tvTitleDescription.setText(movie.getTitle());
+        tvYear.setText(getString(R.string.movie_release_year_title));
+        tvYearDescription.setText(String.valueOf(movie.getReleaseYear()));
+        tvGenre.setText(getString(R.string.movie_genre_title));
+        tvGenreDescription.setText(presenter.buildGenreList(movie.getGenreList()));
+
         tvRating.setText(String.valueOf(movie.getRating()));
-        tvGenre.setText(presenter.buildGenreList(movie.getGenreList()));
 
         loadImage(movie);
     }
@@ -68,7 +82,6 @@ public class MovieDetailsFragment extends Fragment implements MovieDetailsInterf
 
         Glide.with(MovieDetailsFragment.this)
                 .load(movie.getImageUrl())
-                .override(400, 400)
                 .into(ivMovieImage);
     }
 
